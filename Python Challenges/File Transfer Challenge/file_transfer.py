d75680826b6ee9c265f928b32d767044b9cd61a7
natehill22 <natehill22@gmail.com>
2026-01-19 15:01:04 -0800

    Updating file_transfer assignment

--- Python Challenges/File Transfer Challenge/file_transfer.py
+++ Python Challenges/File Transfer Challenge/file_transfer.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import *
 import tkinter.filedialog
+import os
+import shutil
+from datetime import datetime, timedelta
 
 
 class ParentWindow(Frame):
@@ -9,13 +12,15 @@ class ParentWindow(Frame):
 
         self.master = master
         self.master.resizable(width=True, height=True) #Makes the window resizable
-        self.master.geometry('{}x{}'.format(650, 130)) #Sets the size of the frame (Width, Height)
+        self.master.geometry('{}x{}'.format(650, 150)) #Sets the size of the frame (Width, Height)
         self.master.title("File Transfer") #Sets the title of the TKinter module
         self.master.config(bg='#F2F2F2') #Sets the background color
         self.master.grid_rowconfigure(0, weight=1) #Sets the weight of rows and columns so that columnspan functions more ideally
         self.master.grid_rowconfigure(1, weight=1)
+        self.master.grid_rowconfigure(2, weight=1)
         self.master.grid_columnconfigure(0, weight=1)
         self.master.grid_columnconfigure(1, weight=1)
+        self.master.grid_columnconfigure(2, weight=1)
 
 
         #Creates button to select files from source directory and sets location on GUI
@@ -34,14 +39,21 @@ class ParentWindow(Frame):
         self.destination_dir = Entry(width=75)
         self.destination_dir.grid(row=1, column=1, columnspan=2, padx=(20, 10), pady=(15, 10))
 
+        #Creates button to transfer files and sets GUI location
+        self.transfer_btn = Button(text="Transfer Files", width=20, command=self.transferFiles)
+        self.transfer_btn.grid(row=2, column=1, padx=(200, 0), pady=(0, 15))
+
+        #Creates Exit button and sets GUI location
+        self.exit_btn = Button(text="Exit", width=20, command=self.exit_program)
+        self.exit_btn.grid(row=2, column=2, padx=(10, 40), pady=(0, 15))
+
 
 
     #Selects source directory
     def sourceDir(self):
         selectSourceDir = tkinter.filedialog.askdirectory()
         self.source_dir.delete(0, END) #Clears the content inserted in the Entry widget, to allow the path to be inserted instead
-        self.source_dir.insert(0, selectSourceDir) #Places the user's selection to the source_dir field
-            
+        self.source_dir.insert(0, selectSourceDir) #Places the user's selection to the source_dir field  
 
     #Selects destination directory
     def destDir(self):
@@ -49,12 +61,25 @@ class ParentWindow(Frame):
         self.destination_dir.delete(0, END) #Clears the content inserted in the Entry widget, to allow the path to be inserted instead
         self.destination_dir.insert(0, selectDestDir) #Places the user's selection to the destination field
 
+    #Transfer files from one directory to another
+    def transferFiles(self):
+        source = self.source_dir.get() #Gets source directory
+        destination = self.destination_dir.get() #Gets destination directory
+        source_files = os.listdir(source) #Gets list of files in source directory
+        for i in source_files: #Runs through each file in source directory
+            crude_mod_timestamp = os.path.getmtime(source + '/' + i) #Creates a variable for the last-modified timestamp for the selected file
+            file_mod_timestamp = datetime.fromtimestamp(crude_mod_timestamp) #Formats that timestamp to match the other datetime formats
+            current_time = datetime.now() #Create a variable for the current time
+            one_day_ago = current_time - timedelta(days = 1) #Creates a variable for exactly 24 hours ago
+            if file_mod_timestamp < current_time and file_mod_timestamp > one_day_ago: #If the selected file timestamp is between the current time and 1 day ago, 
+                shutil.move(source + '/' + i, destination) #Moves each file from source to destination
+                print(i + ' was successfully transferred.')
+            else: #Otherwise print an alternative message
+                print('All new files (added within the last day) have been transferred. If your file has not been moved, it will need to be modified. ')
 
-
-
-
-
-
+    #Exits the program
+    def exit_program(self):
+        root.destroy() #Root = main GUI window. Tkinter destroy method terminates root.mainloop and all widgets in window
 
 
 
