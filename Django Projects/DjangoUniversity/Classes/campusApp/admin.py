978098fbf31bc63a3e2df50f75040b5d5ecb149a
natehill22 <natehill22@gmail.com>
2026-01-30 13:05:27 -0800

    Adding comments to admin.py

--- Django Projects/DjangoUniversity/Classes/campusApp/admin.py
+++ Django Projects/DjangoUniversity/Classes/campusApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import UniversityCampus
 
+#Registers the imported model to make it visible in the admin panel
 admin.site.register(UniversityCampus)
 
 
--- Django Projects/DjangoUniversity/Classes/classApp/admin.py
+++ Django Projects/DjangoUniversity/Classes/classApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import UniversityClasses
 
+#Registers the imported model to make it visible in the admin panel
 admin.site.register(UniversityClasses)
 
 
